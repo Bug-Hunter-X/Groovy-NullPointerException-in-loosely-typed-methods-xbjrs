57750742f4467d641c47dfc4664721c760886d00
BugHunter-X <askzenai@gmail.com>
2024-12-09 05:06:06 +0100

    Add bug.groovy file

---
+++ bug.groovy
@@ -0,0 +1,11 @@
+def myMethod(a, b) {
+  if (a == null || b == null) {
+    return null
+  }
+  return a + b
+}
+
+println myMethod(1,2) //3
+println myMethod(null,2) //null
+println myMethod(1,null) //null
+println myMethod(null,null) //null
